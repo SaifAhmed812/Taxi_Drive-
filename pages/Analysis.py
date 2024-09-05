3d1bf38c1a25a263e3c3009437e16131867d17c3
ahmedsalem6686@gmail.com <saifeldinahmedlearn@hmail.com>
2024-09-05 20:28:21 +0300

    LFS

---
+++ pages/Analysis.py
@@ -0,0 +1,334 @@
+import streamlit as st
+import seaborn as sns
+import time
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+
+
+df = pd.read_csv("E:\Epsilon_ai\Final_Project\Data_readyy_to_streamlit_downsampled")
+
+df['pickup_date'] = pd.to_datetime(df['pickup_date'])
+df['pickup_time'] = pd.to_timedelta(df['pickup_time'])
+
+df['dropoff_date'] = pd.to_datetime(df['dropoff_date'])
+df['dropoff_time'] = pd.to_timedelta(df['dropoff_time'])
+
+df2 = df.sample(frac=0.1, random_state=42)  # Sampling 10% of the data
+df2['pickup_date'] = pd.to_datetime(df2['pickup_date'])
+df2['pickup_time'] = pd.to_timedelta(df2['pickup_time'])
+
+df2['dropoff_date'] = pd.to_datetime(df2['dropoff_date'])
+df2['dropoff_time'] = pd.to_timedelta(df2['dropoff_time'])
+
+st.markdown("<h1 style='text-align: center; color: white;'>Choose a category</h1>", unsafe_allow_html=True)
+value = st.selectbox("",["Univariant Analysis","Bivariant Analysis"])
+
+if value == "Univariant Analysis":
+#============================================================ Average Distances of the travels
+    
+    st.markdown("<h1 style='text-align: center; color: white;'>Average Distances of the travels</h1>", unsafe_allow_html=True)
+    fig2,ax1 =  plt.subplots()
+    plt.figure(figsize=(10, 6))
+    ax1.hist(df2.trip_distance, bins=50, edgecolor='black')
+    st.pyplot(fig2)
+    
+
+    
+#============================================================ Payment types   
+    st.markdown("<h1 style='text-align: center; color: white;'>Payment types </h1>", unsafe_allow_html=True)
+    plt.figure(figsize=(10, 6))
+    st.plotly_chart(px.histogram(df['payment_type']))
+    
+    
+    
+#============================================================ Tip_Amount based on the Payment of Customer
+
+    st.markdown("<h1 style='text-align: center; color: white;'>Tip_Amount based on the Payment of Customer</h1>", unsafe_allow_html=True)
+    plt.figure(figsize=(10, 6))
+    st.plotly_chart(px.histogram(df, x='tip_amount',color='payment_type',nbins = 15))
+
+    
+
+#============================================================ Demand over the week
+
+
+    st.markdown("<h1 style='text-align: center; color: white;'>Demand over the week</h1>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    df['day_of_week'] = df['pickup_date'].dt.day_name()
+    plt.figure(figsize=(10, 6))
+    sns.countplot(x='day_of_week', data=df, order=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],ax =ax)
+    st.pyplot(fig)
+
+    plt.figure(figsize=(8, 6))
+    st.plotly_chart(px.pie( values = df.day_of_week.value_counts(normalize = True) *100,names =df.day_of_week.unique() , hole = 0.3,
+       color_discrete_sequence=px.colors.sequential.RdBu))
+    
+    
+#============================================================ 'The Busiest Month'  
+    
+    st.markdown("<h1 style='text-align: center; color: white;'>The Busiest Month</h1>", unsafe_allow_html=True)
+    fig2,ax1 =  plt.subplots()
+    plt.figure(figsize=(10, 6))
+    ax1.hist(df.pickup_date.dt.month, bins=15, edgecolor='black')
+    plt.xlabel('Month')
+    plt.ylabel('Frequent')
+    plt.title('The Busiest Month')
+    plt.xticks(range(1, 13))
+    plt.grid(axis='y', alpha=0.5)
+    st.pyplot(fig2)  
+
+    
+    
+    
+    
+    
+    
+#============================================================ Demand By Location
+
+    st.markdown("<h1 style='text-align: center; color: white;'>Taxi Demand by Location over the year</h1>", unsafe_allow_html=True)
+    fig, ax = plt.subplots(figsize=(50, 8))
+    sns.countplot(x='pick_up_location', data=df, ax=ax)
+    ax.set_xlabel('Pick-Up Locations')
+    ax.set_ylabel('Number of Trips')
+    plt.xticks(rotation=90)
+
+    st.pyplot(fig)
+
+
+#============================================================ The Top 10 Frequent pick_up_locations    
+
+    Top_pick_up = df.pick_up_location.value_counts(normalize = True ,ascending = False ).head(10).keys()
+    Percentage = df.pick_up_location.value_counts(normalize = True ,ascending = False ).head(10).tolist()
+    
+    mapping = dict(zip(Percentage, Top_pick_up))
+    
+    dfx = pd.DataFrame()
+    dfx["X"] = Top_pick_up
+    dfx["Y"] = Percentage
+    
+#========================   
+    fig = px.bar(dfx, x='X', y='Y', title="The Most Frequent Pick-Up Locations")
+
+    # Update layout and axis titles
+    fig.update_xaxes(title_text="Areas", tickangle=90)
+    fig.update_yaxes(title_text="Percentage")
+    fig.update_layout(
+        title=dict(
+            text="The Top 10 Frequent Pick-Up Locations",
+            font=dict(size=24)
+        ),
+        xaxis_title=dict(
+            text="Percentage of the Trips",
+            font=dict(size=18)
+        ),
+        yaxis_title=dict(
+            text="Areas",
+            font=dict(size=18)
+        )
+    )
+
+    st.plotly_chart(fig)
+        
+#============================================================ Demand By Location      
+    fig, axs = plt.subplots(1, 2, figsize=(20, 6))
+    
+    st.markdown("<h1 style='text-align: center; color: white;'>Fare amount Vs Pick up locations </h1>", unsafe_allow_html=True)
+    # First subplot: Bar plot
+    df.groupby('pick_up_location').fare_amount.mean().sort_values().head(15).plot(kind='bar', ax=axs[0])
+    axs[0].set_xlabel('Pick-Up Location')
+    axs[0].set_ylabel('Average Fare Amount')
+    axs[0].set_title('Average Fare Amount by Pick-Up Location')
+
+   
+    
+    # Second subplot: Pie chart
+    
+    axs[1].pie(df.groupby('pick_up_location').fare_amount.mean().sort_values().head(15),
+           labels=df.groupby('pick_up_location').fare_amount.mean().sort_values().head(15).keys(),
+           autopct='%1.1f%%',
+           startangle=180)
+
+    axs[1].axis('equal')
+    plt.tight_layout()
+    st.pyplot(fig)
+    
+    
+    
+    
+#============================================================ The Top 10 Frequent Drop_Off_locations
+
+    st.markdown("<h1 style='text-align: center; color: white;'>The Top 10 Frequent Drop_Off_locations </h1>", unsafe_allow_html=True)
+    
+    labels_drop_off = df.drop_of_location.value_counts(normalize = True ,ascending = False ).head(10).keys()
+
+    ValuesOfDropOff = df.drop_of_location.value_counts(normalize = True ,ascending = False ).head(10).tolist()
+
+    mapping = dict(zip(ValuesOfDropOff, labels_drop_off))
+
+    dfx_drop = pd.DataFrame()
+    dfx_drop["x"] = ValuesOfDropOff
+    dfx_drop["y"] = labels_drop_off
+
+
+#===============
+
+    fig = px.bar(dfx_drop, x='x', y='y', title="The Most Frequent Pick-Up Locations")
+
+    # Update layout and axis titles
+    fig.update_xaxes(title_text="Areas", tickangle=90)
+    fig.update_yaxes(title_text="Percentage")
+    fig.update_layout(
+        title=dict(
+            text="The Top 10 Frequent Drop_Off_locations",
+            font=dict(size=24)
+        ),
+        xaxis_title=dict(
+            text="Percentage of the Trips",
+            font=dict(size=18)
+        ),
+        yaxis_title=dict(
+            text="Areas",
+            font=dict(size=18)
+        )
+    )
+
+    st.plotly_chart(fig)  
+
+#===============
+
+
+    
+    plt.figure(figsize=(12, 6))
+    
+    df['drop_of_location'].value_counts().sort_values(ascending=False).head(15).plot(kind='bar')
+
+    # Set titles and labels
+    plt.title('Top 15 Drop-Off Locations')
+    plt.xlabel('Drop-Off Location')
+    plt.ylabel('Number of Trips')
+    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+
+
+    st.pyplot(plt)
+
+
+elif value == "Bivariant Analysis":
+    
+    
+#============================================================ Fare amount versus Trip distance
+
+    # Plotting correlation between trip distance and fare amount
+    st.markdown("<h1 style='text-align: center; color: white;'>Fare amount versus Trip distance </h1>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x='trip_distance', y='fare_amount', data=df, ax =ax)
+    plt.xlabel('Trip Distance (miles)')
+    plt.ylabel('Fare Amount ($)')
+    st.pyplot(fig)
+
+    
+
+#============================================================ Correlation graph
+    st.markdown("<h1 style='text-align: center; color: white;'>Correlation graph</h1>", unsafe_allow_html=True)
+
+    df3 = df.select_dtypes('float','int')
+    df3.drop(columns = ["mta_tax","tolls_amount"],axis =1, inplace = True)
+
+    num_cols = df3.select_dtypes('float','int')
+#========================    
+    fig, ax = plt.subplots()
+    plt.figure(figsize=(12,6))
+    sns.heatmap(num_cols.corr(), annot=True, fmt=".2f", cmap='coolwarm', linewidths=0.5 , ax= ax)
+    st.pyplot(fig)
+
+
+
+#============================================================ fare amount over the months   
+
+    df['pickup_date'] = pd.to_datetime(df['pickup_date'])
+
+    df['month'] = df['pickup_date'].dt.to_period('M').dt.to_timestamp()
+
+    st.markdown("<h1 style='text-align: center; color: white;'>Fare Amount Over the Months</h1>", unsafe_allow_html=True)
+
+  
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(x=df['month'], y=df['fare_amount'], marker='o', linestyle='-', ax=ax)
+    ax.set_xlabel('Month')
+    ax.set_ylabel('Fare Amount')
+    ax.set_title('Fare Amount Over Time')
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45)
+
+    # Adjust layout to prevent clipping of labels
+    plt.tight_layout()
+  
+    st.pyplot(fig)
+    plt.clf()
+
+#============================================================ Tip amount over Time
+
+#    st.markdown("<h1 style='text-align: center; color: white;'> Tip amount over Time </h1>", unsafe_allow_html=True)
+    
+#    df['pickup_date'] = pd.to_datetime(df['pickup_date'])
+  
+# #   df['month'] = df['pickup_date'].dt.to_period('M').dt.to_timestamp()
+
+#    # Create the plot
+# #   fig, ax = plt.subplots(figsize=(15, 6))
+
+    # Create a line plot with Seaborn
+# #   sns.lineplot(x=df['month'], y=df['tip_amount'], marker='o', linestyle='-', ax=ax)
+
+    # Set titles and labels
+#  #  ax.set_title('Monthly Tip Amounts')
+#  #  ax.set_xlabel('Month')
+#  #  ax.set_ylabel('Tip Amount')
+
+    # Rotate x-axis labels for better readability
+#  #  plt.xticks(rotation=45, ha='right')
+
+    # Adjust layout to prevent clipping of labels
+#  #  plt.tight_layout()
+
+#  #  st.pyplot(fig)
+## #   plt.clf()
+
+#============================================================ Average tip amount per month
+
+
+    st.markdown("<h1 style='text-align: center; color: white;'> Average Tip amount over Time</h1>", unsafe_allow_html=True)
+
+    df['pickup_date'] = pd.to_datetime(df['pickup_date'])
+
+    # Create 'month' column as Period and then convert to Timestamp for plotting
+    df['month'] = df['pickup_date'].dt.to_period('M')
+
+    # Calculate average tip amount per month
+    monthly_avg_tips = df.groupby('month')['tip_amount'].mean().reset_index()
+
+    # Convert 'month' back to datetime for plotting
+    monthly_avg_tips['month'] = monthly_avg_tips['month'].dt.to_timestamp()
+
+
+    # Create the plot
+    fig, ax = plt.subplots(figsize=(15, 6))
+    sns.lineplot(x='month', y='tip_amount', data=monthly_avg_tips, marker='o', linestyle='-', ax=ax)
+    ax.set_xlabel('Month')
+    ax.set_ylabel('Average Tip Amount')
+   
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45, ha='right')
+    # Adjust layout to prevent clipping of labels
+    plt.tight_layout()
+
+    # Display the plot in Streamlit
+    st.pyplot(fig)
+
+    # Optionally, if you want to clear the plot after displaying:
+    plt.clf()
+
+
---
+++ pages/Book Your Travel.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import joblib
+from datetime import datetime, timedelta
+
+# Load preprocessor and model
+preprocessor = joblib.load('preprocessor.pkl')
+model = pickle.load(open("E:\Epsilon_ai\Final_Project\model_RD", "rb"))
+
+# Load your dataframes
+df = pd.read_csv("E:\Epsilon_ai\Final_Project\_Cleaned_Taxi_Datset.csv")
+dfx = pd.read_csv("E:\Epsilon_ai\Final_Project\_Cleaned_locations.csv")
+dfx2 = pd.read_csv("E:\Epsilon_ai\Final_Project\Locations and codes.csv")
+
+# Get unique locations
+unique_pickup_locations = dfx['Column1'].unique().tolist()
+unique_dropoff_locations = dfx['Column2'].unique().tolist()
+
+st.title("Taxi Fare Prediction")
+
+def get_payment_code(value):
+    payment_codes = {'cash': 1, 'credit': 2, 'mobile': 3, 'prcard': 4}
+    return payment_codes.get(value, None)
+
+def get_code_from_location(location):
+    result = dfx2.loc[dfx2['pick_up_location'] == location, 'pulocationid']
+    if not result.empty:
+        return result.iloc[0]
+    else:
+        st.error(f"Location {location} not found")
+        return None
+
+def get_trip_distance(pick_up_location, drop_of_location):
+    trip_distance = dfx[
+        (dfx['Column1'] == pick_up_location) & (dfx['Column2'] == drop_of_location)
+    ]['Column3'].values
+    if trip_distance.size > 0:
+        return trip_distance[0]
+    else:
+        return None
+
+def get_trip_duration(current_hour, dropoff_hour):
+    now = datetime.now()
+    pick_up_time = now.replace(hour=current_hour, minute=0, second=0, microsecond=0)
+    dropoff_time = now.replace(hour=dropoff_hour, minute=0, second=0, microsecond=0)
+
+    # If drop-off time is earlier than pick-up time, assume it's on the next day
+    if dropoff_time < pick_up_time:
+        dropoff_time += timedelta(days=1)
+
+    duration = dropoff_time - pick_up_time
+    total_seconds = int(duration.total_seconds())
+
+    hours = total_seconds // 3600
+    seconds = total_seconds % 3600
+
+    return hours, seconds
+
+with st.form(key="form1"):
+    passenger_count = st.number_input("Passenger Count", 1, 5)
+    Tip = st.number_input("Tip amount", min_value=0.0)
+    pick_up_location = st.selectbox("Select Pick-Up Location", options=unique_pickup_locations)
+    drop_of_location = st.selectbox("Select Drop-Off Location", options=unique_dropoff_locations)
+    payment_type = st.selectbox("Payment Type", ["credit", "cash", "mobile", "prcard"])
+
+    submit = st.form_submit_button(label="Submit")
+
+    if submit:
+        now = datetime.now()
+        current_hour = now.hour
+
+        code = get_payment_code(payment_type)
+        drop_location = get_code_from_location(drop_of_location)
+        pick_location = get_code_from_location(pick_up_location)
+
+        if drop_location is None or pick_location is None:
+            st.error("Invalid pick-up or drop-off location.")
+        else:
+            trip_distance = get_trip_distance(pick_up_location, drop_of_location)
+
+            if trip_distance is None:
+                trip_distance = 1.7510  # mean value
+            else:
+                trip_distance = float(trip_distance)
+
+            new_data = {
+                'passenger_count': passenger_count,
+                'trip_distance': trip_distance,
+                'ratecodeid': 1,
+                'payment_type': code,
+                "extra": 1.7412 ,#mean
+                "mta_tax": 0.5,
+                "tip_amount": Tip,
+                'congestion_surcharge': 2.422, #mean
+                'pick_up_location': pick_location,
+                'drop_of_location': drop_location,
+                'pick_up_day': now.day,
+                'pickup_hour': current_hour
+            }
+
+            new_data_df = pd.DataFrame(new_data, index=[0])
+
+            if new_data_df.isna().any().any() or (new_data_df == float('inf')).any().any():
+                st.error("Input data contains NaN or infinite values.")
+            else:
+                # Preprocess the new data
+                new_data_preprocessed = preprocessor.transform(new_data_df)
+
+                # Predict using the model
+                try:
+                    y_pred = model.predict(new_data_preprocessed)
+
+                    # Assuming y_pred is an array with the first element being the fare amount
+                    fare_amount = y_pred[0][0]  # Extract fare_amount
+                    dropoff_hour = y_pred[0][1]  # Extract dropoff_hour
+
+                    hours, seconds = get_trip_duration(current_hour, int(dropoff_hour))
+                    if pick_up_location ==  drop_of_location :
+                            fare_amount = 0.00
+
+                    st.markdown('# Price in USD:')
+                    st.markdown(f'## **${fare_amount:,.2f}**')
+                    
+
+                except Exception as e:
+                    st.error(f"An error occurred during prediction: {e}")
